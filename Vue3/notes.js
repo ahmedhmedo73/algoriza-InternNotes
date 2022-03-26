920e89317740f388ef2515695dc79d95fdfa9895
ahmed kamel <58473744+ahmedhmedo73@users.noreply.github.com>
2022-03-26 10:57:02 +0200

    task

---
+++ Vue3/notes.js
@@ -0,0 +1,18 @@
+/* Binding 
+
+   //Property
+   <p>{{var}}</p>
+   
+   //Attribute
+   <img :src="image">    put ":"  before attribute
+
+   //Conditional Rendering
+
+   <p v-if="inStock">In Stock</p>
+   <p v-else>Out of Stock</p>
+
+   <p v-show="inStock">In Stock</p>    // give element display:none;
+
+   
+
+*/
